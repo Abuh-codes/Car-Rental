dec08572d8d2a066904df33e6f28b75e29f8c0ec
Abuh-codes <ak.abrahamkibet@gmail.com>
2025-10-21 22:49:23 +0300

    FEAT: Car-Cards

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -12,7 +12,7 @@ export const Hero = () => {
         <div className="flex flex-col md:flex-row items-start md:items-center gap-10 min-md:ml-8">
           <div className="flex flex-col items-start gap-2">
             <select
-              require
+              required
               value={pickupLocation}
               onChange={(e) => setPickupLocation(e.target.value)}
             >
@@ -27,7 +27,34 @@ export const Hero = () => {
               {pickupLocation ? pickupLocation : "Please select location"}
             </p>
           </div>
+          <div className="flex flex-col items-start gap-2">
+            <label htmlFor="pickup-date">Pick-up Date</label>
+            <input
+              type="date"
+              id="pickup-date"
+              min={new Date().toISOString().split("T")[0]}
+              className="text-sm text-gray-500"
+              required
+            />
+          </div>
+          <div className="flex flex-col items-start gap-2">
+            <label htmlFor="return-date">Return Date</label>
+            <input
+              type="date"
+              id="return-date"
+              className="text-sm text-gray-500"
+              required
+            />
+          </div>
         </div>
+        <button className="flex items-center justify-center gap-1 px-9 py-3 max-sm:mt-4 bg-primary hover:bg-primary-dull text-white rounded-full cursor-pointer">
+          <img
+            src={assets.search_icon}
+            alt="search"
+            className="brightness-300"
+          />
+          Search
+        </button>
       </form>
       <img src={assets.main_car} alt="car" className="max-h-74" />
     </div>
